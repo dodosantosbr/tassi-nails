5940f77321fa49db4090fe646d69448a207b2649
Douglas Sales <sallesard@gmail.com>
2026-03-24 19:50:31 -0300

    correção do supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://edlauwvnlnxbkaiovmal.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkbGF1d3ZubG54YmthaW92bWFsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzODk1NDEsImV4cCI6MjA4OTk2NTU0MX0.kqpXSqDie2QfAJSFZSBkAe8YeW-Hrh-nPw7idmlkfbo";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(supabaseUrl, supabaseKey);
