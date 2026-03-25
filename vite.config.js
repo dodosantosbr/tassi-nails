23debc3c70e454fb34ed40cb03b3856d3a178381
Douglas Sales <sallesard@gmail.com>
2026-03-24 21:45:27 -0300

    pwa funcionando

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+
+      devOptions: {
+        enabled: true, // 👈 permite testar PWA no npm run dev
+      },
+
+      manifest: {
+        name: "NailStudio Pro",
+        short_name: "NailStudio",
+        description: "Sistema de agendamento para Nail Designer 💅",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#e11d48",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
